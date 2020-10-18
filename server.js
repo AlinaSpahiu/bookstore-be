76a7491f8357700d062fb9bf0087f8710b8c65ca
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-10-18 22:24:09 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
+const{ notFound, errorHandler} = require('./middleware/errorMiddleware')
+const bookRoutes = require('./routes/bookRoutes')
+const userRoutes = require('./routes/userRoutes')
+
+
+dotenv.config()
+connectDB()
+const server = express()
+
+// Routes
+server.use('/api/books', bookRoutes)
+// server.use('/api/users', userRoutes)
+
+// MiddleWares:
+server.use(notFound)
+server.use(errorHandler)
+
+const PORT = process.env.PORT || 5001
+
+server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
