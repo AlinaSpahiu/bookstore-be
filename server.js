688b69443b4bb999aa179703a1e2df026e11045a
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-20 20:09:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ server.use('/api/users', userRoutes)
 server.use(notFound)
 server.use(errorHandler)
 
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5003
 
 server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
