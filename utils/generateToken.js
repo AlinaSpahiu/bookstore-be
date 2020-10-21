af61b2459289ea9123db4fc8f904b0efc7a466b7
AlinaSpahiu <alina.s.abbaz@gmail.com>
2020-10-22 01:04:48 +0200

    Update generateToken.js

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,9 @@
 const jwt = require('jsonwebtoken')
 
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: 9999})
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: 9999,
+  })
 }
 
 module.exports = generateToken
