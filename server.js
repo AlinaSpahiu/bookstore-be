48612909814f1fdc89f7d01225f6832072f2fcda
AlinaSpahiu <58088283+AlinaSpahiu@users.noreply.github.com>
2020-10-26 22:53:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const cors = require("cors")
 const connectDB = require('./config/db')
 const{ notFound, errorHandler} = require('./middleware/errorMiddleware')
 const bookRoutes = require('./routes/bookRoutes')
@@ -9,16 +10,18 @@ const userRoutes = require('./routes/userRoutes')
 dotenv.config()
 connectDB()
 const server = express()
+server.use(cors())
 server.use(express.json())
 
 // Routes
 server.use('/api/books', bookRoutes)
-server.use('/api/users', userRoutes)
+server.use("/api/users", userRoutes)
 
 // MiddleWares:
 server.use(notFound)
+
 server.use(errorHandler)
 
-const PORT = process.env.PORT || 5003
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
